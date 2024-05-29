d7a14cc752ff8f4bbfe372a851bf69fdd53bdf98
DeanAvery7088 <Brooks88@yahoo.com>
2024-05-28 23:26:04 -0500

    Main Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 500f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    // Process the inputs of the player
+    void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            // To make force applied frame rate independent, multiplying by Time.deltaTime 
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) | Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow) | Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        // Deactivate Unity's physics to provide player control 
+        rb.freezeRotation = true;  
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        // Reactivate Unity's physics when no longer requested by the player
+        rb.freezeRotation = false;  
+    }
+}
